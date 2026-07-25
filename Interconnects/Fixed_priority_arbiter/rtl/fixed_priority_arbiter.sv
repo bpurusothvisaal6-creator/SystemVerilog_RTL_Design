f6cc3e831083adb5cf2a501ec3f19a11cca0e7b0
bpurusothvisaal6-creator <bpurusothvisaal6@gmail.com>
2026-07-26 01:30:33 +0530

    Update fixed_priority_arbiter.sv

--- Interconnects/Fixed_priority_arbiter/rtl/fixed_priority_arbiter.sv
+++ Interconnects/Fixed_priority_arbiter/rtl/fixed_priority_arbiter.sv
@@ -1,1 +1,21 @@
+module fixed_priority_arbiter #(
+    parameter int NUM_REQ = 8   // number of requesters
+) (
+    input  logic [NUM_REQ-1:0] req,    // request vector, active high
+    output logic [NUM_REQ-1:0] grant   // one-hot grant vector
+);
+
+    always_comb begin
+        logic found;
+        grant = '0;
+        found = 1'b0;
+        for (int i = 0; i < NUM_REQ; i++) begin
+            if (req[i] && !found) begin
+                grant[i] = 1'b1;
+                found    = 1'b1;
+            end
+        end
+    end
+
+endmodule
 
